d05356a964408124fc6e4dee447547b45f8900f2
Savan <savanvachhani080192@gmail.com>
2026-04-06 12:27:25 +0100

    w3school

--- python_w3school/hello.py
+++ python_w3school/hello.py
@@ -1,6 +1,11 @@
 import sys
 print(sys.version)
 print("Hello, World!")
+"""
+This is a comment
+written in
+more than just one line
+"""
 print("Have a good day.")
 print("Learning Python is fun!")
 #This is a comment.
